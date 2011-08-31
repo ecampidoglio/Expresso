12d902769d937e178b89c3afe8efa40c719a36e4
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2011-08-31 21:42:51 +0200

    NEW: Added first implementation of the 'ProductsController'.

---
+++ Thoughtology.Expresso.Tests/Foundation/AutoMoqDataAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit;
+
+namespace Thoughtology.Expresso.Tests.Foundation
+{
+    /// <summary>
+    /// Provides auto-generated data specimens to xUnit Theories
+    /// using AutoFixture for concrete types and Moq for interfaces and abstract classes.
+    /// </summary>
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoMoqDataAttribute"/> class.
+        /// </summary>
+        public AutoMoqDataAttribute()
+            : base(new Fixture().Customize(
+                new AutoMoqCustomization()))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoMoqDataAttribute"/> class.
+        /// </summary>
+        /// <param name="fixtureType">Type of the fixture.</param>
+        public AutoMoqDataAttribute(Type fixtureType)
+            : base(((IFixture)Activator.CreateInstance(fixtureType)).Customize(
+                new AutoMoqCustomization()))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoMoqDataAttribute"/> class.
+        /// </summary>
+        /// <param name="fixture">The <see cref="IFixture"/> instance.</param>
+        public AutoMoqDataAttribute(IFixture fixture)
+            : base(fixture.Customize(
+                new AutoMoqCustomization()))
+        {
+        }
+    }
+}
--- Thoughtology.Expresso.Tests/Web/Controllers/ProductsControllerTest.cs
+++ Thoughtology.Expresso.Tests/Web/Controllers/ProductsControllerTest.cs
@@ -1,22 +1,62 @@
-﻿using Xunit;
-using System;
+﻿using System;
+using System.Web.Mvc;
+using Thoughtology.Expresso.Tests.Foundation;
+using Thoughtology.Expresso.Web.Controllers;
+using Xunit;
+using Xunit.Extensions;
 
 namespace Thoughtology.Expresso.Tests.Web.Controllers
 {
-    public class ProductsController
-    {
-        public ProductsController(object productService)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public class ProductsControllerTest
     {
         [Fact]
         public void Constructor_WithNull_ThrowsArgumentNullException()
         {
             Assert.Throws(typeof(ArgumentNullException), () => new ProductsController(null));
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void Index_DoesNotReturnNull(object productService)
+        {
+            var sut = new ProductsController(productService);
+
+            var result = sut.Index();
+
+            Assert.NotNull(result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void Index_ReturnsEmptyResult(object productService)
+        {
+            var sut = new ProductsController(productService);
+
+            var result = sut.Index();
+
+            Assert.IsType<EmptyResult>(result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void Coffee_DoesNotReturnNull(object productService)
+        {
+            var sut = new ProductsController(productService);
+
+            var result = sut.Coffee();
+
+            Assert.NotNull(result);
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void Coffee_ReturnsViewResult(object productService)
+        {
+            var sut = new ProductsController(productService);
+
+            var result = sut.Coffee();
+
+            Assert.IsType<ViewResult>(result);
         }
     }
 }
