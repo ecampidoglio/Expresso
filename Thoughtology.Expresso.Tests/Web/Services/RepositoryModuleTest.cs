cb6a5040eb8141e8d4214327bb663815e98b4603
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2011-09-21 14:20:47 +0200

    FIX: taken dependency on AutoFac. Upgraded to AutoFixture 2.4.

---
+++ Thoughtology.Expresso.Tests/Web/Services/RepositoryModuleTest.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using System;
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Registration;
+using System.Linq;
+using Thoughtology.Expresso.Model;
+using Thoughtology.Expresso.Data;
+
+namespace Thoughtology.Expresso.Tests.Web.Services
+{
+    public class RepositoryModule : IModule
+    {
+        public void Configure(IComponentRegistry componentRegistry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RepositoryModuleTest
+    {
+        [Fact]
+        public void Constructor_RegistersOpenRepositories()
+        {
+            // Given
+            var builder = new ContainerBuilder();
+            var sut = new RepositoryModule();
+
+            // When
+            builder.RegisterModule(sut);
+            var container = builder.Build();
+            var postRepo = container.Resolve<IRepository<Post>>();
+
+            // Then
+            Assert.NotNull(postRepo);
+        }
+    }
+}
