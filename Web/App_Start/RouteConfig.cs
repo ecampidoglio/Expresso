1ac6fbcda9e79e413bda94da802910f75b046bbe
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-11-29 00:29:54 +0100

    Adds route to pass parameters to directly to the default action.

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -9,8 +9,13 @@ namespace Thoughtology.Expresso.Web
         {
             routes.MapRoute(
                 name: "Default",
+                url: "{controller}/{id}",
+                defaults: new { controller = "Post", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                name: "Action",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Post", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
