468420ad60c9e437737640c2ff7f878454068061
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-11-28 17:26:08 +0100

    Adds bootstrapper for the Database deployment package.

---
+++ Database/Deploy.ps1
@@ -0,0 +1,9 @@
+﻿# This script is automatically invoked by Octopus Deploy
+# to perform the deployment.
+# Other scripts that will be invoked as well, if present:
+# - PreDeploy.ps1
+# - Deploy.ps1
+# - PostDeploy.ps1
+# - DeployFailed.ps1
+
+& .\Expresso.Database.exe | Write-Host
