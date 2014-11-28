32485c61747d499af5a432cea43c2cd6956eb0cb
Enrico Campidoglio <enrico.campidoglio@gmail.com>
2014-11-28 16:07:22 +0100

    Adds empty database migrations project.

---
+++ Database/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Configuration;
+using System.Reflection;
+using DbUp;
+using DbUp.Engine;
+
+namespace Expresso.Database
+{
+    class Program
+    {
+        static DatabaseUpgradeResult migrationResult;
+
+        static int Main(string[] args)
+        {
+            ApplyMigrations();
+            PrintResult();
+            return StatusCode();
+        }
+
+        private static void ApplyMigrations()
+        {
+            var upgrader = DeployChanges
+                .To
+                .SqlDatabase(GetConnectionString())
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+            migrationResult = upgrader.PerformUpgrade();
+        }
+
+        static void PrintResult()
+        {
+            if (MigrationFailed())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(migrationResult.Error);
+                Console.WriteLine("Migration failed.");
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Migration successful.");
+                Console.ResetColor();
+            }
+        }
+
+        static int StatusCode()
+        {
+            return MigrationFailed() ? -1 : 0;
+        }
+
+        static string GetConnectionString()
+        {
+            return ConfigurationManager
+                .ConnectionStrings["Expresso"]
+                .ConnectionString;
+        }
+
+        static bool MigrationFailed()
+        {
+            return !migrationResult.Successful;
+        }
+    }
+}
---
+++ Database/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Expresso.Database")]
+[assembly: AssemblyCompany("megakemp.com")]
+[assembly: AssemblyCopyright("Creative Commons Attribution-ShareAlike")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d05b6c33-56bc-4ac7-8fc2-257c76247b52")]
